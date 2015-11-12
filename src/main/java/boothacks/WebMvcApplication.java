7a4ed7ae474f3202c3d3ba17605953147c0783b6
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-11-12 17:32:06 -0500

    Fix typo

--- src/main/java/boothacks/WebMvcApplication.java
+++ src/main/java/boothacks/WebMvcApplication.java
@@ -21,7 +21,7 @@ public class WebMvcApplication {
 
 
     @Configuration
-    public class WebConfig extends WebMvcConfigurerAdapter {
+    public static   class WebConfig extends WebMvcConfigurerAdapter {
 
         @Autowired
         private ThymeleafViewResolver thymeleafResolver;
